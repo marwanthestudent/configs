4143a494f8ca9053f1e9c036d90e4c8f86b37d7a
marwanthestudent <abulibdeh43@gmail.com>
2025-07-25 16:23:33 -0500

    dashboard

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,19 +1,19 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      flavour = "mocha", -- latte, frappe, macchiato, mocha
-      color_overrides = {
-        mocha = {
-          base = "#22272e",  -- changes the default background color
-          mantle = "#22272e",
-          crust = "#1e222a",
-          text = "#e6e6e6",
-        },
-      },
-    })
-    vim.cmd.colorscheme("catppuccin")
-   end
+   "catppuccin/nvim",
+   name = "catppuccin",
+   priority = 1000,
+   config = function()
+      require("catppuccin").setup({
+         flavour = "mocha", -- latte, frappe, macchiato, mocha
+         color_overrides = {
+            mocha = {
+               base = "#2e2e33", -- main editor background
+               mantle = "#2a2a2f", -- sidebars, panels
+               crust = "#252529", -- statusline, floating windows
+               text = "#e6e6e6",
+            },
+         },
+      })
+      vim.cmd.colorscheme("catppuccin")
+   end,
 }
