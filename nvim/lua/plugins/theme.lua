6a3d3358269c21d47a49cf08c7fa4184e1fc0c93
marwanthestudent <abulibdeh43@gmail.com>
2025-07-25 19:11:03 -0500

    changed theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,19 +1,24 @@
 return {
-   "catppuccin/nvim",
-   name = "catppuccin",
+   "navarasu/onedark.nvim",
+   -- "catppuccin/nvim",
+   name = "onedark",
    priority = 1000,
    config = function()
-      require("catppuccin").setup({
-         flavour = "mocha", -- latte, frappe, macchiato, mocha
-         color_overrides = {
-            mocha = {
-               base = "#2e2e33", -- main editor background
-               mantle = "#2a2a2f", -- sidebars, panels
-               crust = "#252529", -- statusline, floating windows
-               text = "#e6e6e6",
-            },
-         },
+      --      require("catppuccin").setup({
+      --         flavour = "mocha", -- latte, frappe, macchiato, mocha
+      --         color_overrides = {
+      --            mocha = {
+      --               base = "#2e2e33", -- main editor background
+      --               mantle = "#2a2a2f", -- sidebars, panels
+      --               crust = "#252529", -- statusline, floating windows
+      --               text = "#e6e6e6",
+      --            },
+      --         },
+      --      })
+
+      require("onedark").setup({
+         style = "warm",
       })
-      vim.cmd.colorscheme("catppuccin")
+      vim.cmd.colorscheme("onedark")
    end,
 }
