7350af00ac4717fd3b2dc489be01910328db1f49
marwanthestudent <abulibdeh43@gmail.com>
2025-07-29 14:59:46 -0500

    edited the dashboard

--- nvim/lua/plugins/smear.lua
+++ nvim/lua/plugins/smear.lua
@@ -4,12 +4,12 @@ return {
       require("smear_cursor").setup({
        stiffness = 0.7,                      -- 0.6      [0, 1]
        trailing_stiffness = 0.5,             -- 0.4      [0, 1]
-       stiffness_insert_mode = 0.7,          -- 0.5      [0, 1]
-       trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
+       stiffness_insert_mode = 0.65,          -- 0.5      [0, 1]
+       trailing_stiffness_insert_mode = 0.65, -- 0.5      [0, 1]
        damping = 0.7,                        -- 0.65     [0, 1]
-       damping_insert_mode = 0.75,            -- 0.7      [0, 1]
+       damping_insert_mode = 0.8,            -- 0.7      [0, 1]
        distance_stop_animating = 0.35,        -- 0.1      > 0
-       time_interval = 14,
+      time_interval = 7,
       })
    end,
 }
