710d3bcd67e1445778acb41ba9b37299600c4884
marwanthestudent <abulibdeh43@gmail.com>
2025-08-09 16:38:35 -0500

    identation change

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,15 +1,15 @@
-
- return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons'},
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-       require('lualine').setup({
-         options = {
-             theme = 'everforest'
-         },
-         sections = {
-            lualine_x = {'encoding', 'filetype'},  --  removed 'fileformat'
-         }
-      })
-    end
- }
+        require("lualine").setup({
+            options = {
+                theme = "everforest",
+                
+            },
+            sections = {
+                lualine_x = { "encoding", "filetype" }, --  removed 'fileformat'
+            },
+        })
+    end,
+}
--- nvim/lua/vim-customizations.lua
+++ nvim/lua/vim-customizations.lua
@@ -1,8 +1,15 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=3")
-vim.cmd("set softtabstop=3")
-vim.cmd("set shiftwidth=3")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
 vim.g.mapleader = " "
+--not working as intended for some reason
+vim.opt.smartindent = true
+vim.opt.cindent = true
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+
 vim.cmd("set number")
 vim.cmd("set relativenumber")
 
@@ -11,9 +18,9 @@ vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
-vim.keymap.set("v", "J",":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("x", "<leader>p", "\"_dP") -- paste over while keeping buffer 
+vim.keymap.set("x", "<leader>p", '"_dP') -- paste over while keeping buffer
 
 vim.keymap.set("n", "<C-u>", "<C-u>ZZ")
 vim.keymap.set("n", "<C-d>", "<C-d>ZZ")
@@ -21,11 +28,11 @@ vim.keymap.set("n", "<C-d>", "<C-d>ZZ")
 --edit current word in all the buffer
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  group = vim.api.nvim_create_augroup('highlight_yank', {}),
-  desc = 'Hightlight selection on yank',
-  pattern = '*',
-  callback = function()
-    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 150}
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = vim.api.nvim_create_augroup("highlight_yank", {}),
+    desc = "Hightlight selection on yank",
+    pattern = "*",
+    callback = function()
+        vim.highlight.on_yank({ higroup = "IncSearch", timeout = 150 })
+    end,
 })
