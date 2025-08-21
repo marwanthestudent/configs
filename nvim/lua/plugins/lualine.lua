49a56dc156347b3bdf33e05b4ce3eb4f62fd7513
marwanthestudent <abulibdeh43@gmail.com>
2025-08-20 22:30:05 -0500

    can't remmember

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+     dependencies= { "nvim-tree/nvim-web-devicons" },
     config = function()
         require("lualine").setup({
             options = {
@@ -13,3 +13,5 @@ return {
         })
     end,
 }
+--hello
+
